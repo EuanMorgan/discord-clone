99e7b9791c5fe89d2993591694b8282c23a36af5
Euan Morgan <euanmorgan48@gmail.com>
2023-10-08 15:51:18 +0100

    feat: chat header and mobile menu

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,48 @@
-const ChannelIdPage = () => {
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    channelId: string;
+    serverId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId: params.serverId,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
   return (
-    <div>
-      <h1>ChannelIdPage</h1>
+    <div className="flex h-full flex-col bg-white dark:bg-[#313338] ">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      p
     </div>
   );
 };
